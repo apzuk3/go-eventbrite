325bd4b154ed9f9b12860a9bcfad71a7a5b5eccd
Aram Petrosyan <aram.petrosyan.88@gmail.com>
2018-02-22 19:14:39 +0000

    add doc, license

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# go-eventbrite
+# Go client for Eventbrite
+
+[![GoDoc](https://godoc.org/apzuk/go-eventbrite?status.svg)](https://godoc.org/apzuk/go-eventbrite)
+
+
+
---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package maps provides a client library for the Eventbrite API.
+Please see https://www.eventbrite.co.uk/developer/v3/quickstart/ for
+an overview of the Eventbrite API suite.
+*/
+package eventbrite

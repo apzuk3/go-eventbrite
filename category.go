1677a4733796c3b9a7999da61a1268e95cabea76
Aram Petrosyan <aram.petrosyan.88@gmail.com>
2018-02-22 19:21:50 +0000

    add doc

--- category.go
+++ category.go
@@ -6,19 +6,21 @@ import (
 	"golang.org/x/net/context"
 )
 
+// CategoriesResult is the response structure for the Categories
 type CategoriesResult struct {
 	Locale string `json:"locale"`
 	Pagination Pagination `json:"pagination"`
 	Categories []Category `json:"categories"`
 }
 
+// SubCategoriesResult is the response structure for the SubCategories
 type SubCategoriesResult struct {
 	Locale string `json:"locale"`
 	Pagination Pagination `json:"pagination"`
 	Subcategories []Category `json:"subcategories"`
 }
 
-// Returns a list of category as categories, including subcategories nested
+// Categories returns a list of category as categories, including subcategories nested
 //
 // https://www.eventbrite.com/developer/v3/endpoints/categories/#ebapi-get-categories
 func (c *Client) Categories(ctx context.Context) (*CategoriesResult, error) {
@@ -27,7 +29,7 @@ func (c *Client) Categories(ctx context.Context) (*CategoriesResult, error) {
 	return result, c.getJSON(ctx, "/categories", url.Values{}, &result)
 }
 
-// Gets a category by ID as category
+// Category gets a category by ID as category
 //
 // https://www.eventbrite.com/developer/v3/endpoints/categories/#ebapi-get-categories-id
 func (c *Client) Category(ctx context.Context, id string) (*Category, error) {
@@ -36,7 +38,7 @@ func (c *Client) Category(ctx context.Context, id string) (*Category, error) {
 	return result, c.getJSON(ctx, "/categories/nnn/" + id, url.Values{}, &result)
 }
 
-// Returns a list of subcategory as subcategories
+// SubCategories gets a list of subcategory as subcategories
 //
 // https://www.eventbrite.com/developer/v3/endpoints/categories/#ebapi-get-subcategories
 func (c *Client) SubCategories(ctx context.Context) (*SubCategoriesResult, error) {
@@ -45,7 +47,7 @@ func (c *Client) SubCategories(ctx context.Context) (*SubCategoriesResult, error
 	return result, c.getJSON(ctx, "/subcategories/", url.Values{}, &result)
 }
 
-// Gets a subcategory by ID as subcategory
+// SubCategory gets a subcategory by ID as subcategory
 //
 // https://www.eventbrite.com/developer/v3/endpoints/categories/#ebapi-get-subcategories-id
 func (c *Client) SubCategory(ctx context.Context, id string) (*SubCategory, error) {
